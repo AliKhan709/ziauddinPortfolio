751900350ae0a384b1d9b3ac8a1e1afbeb5f86ba
AliKhan709 <100496113+AliKhan709@users.noreply.github.com>
2022-05-26 00:04:21 +0500

    ZiauddinPORTFOLIOWEBSITE

---
+++ Folio - Personal Portfolio Template/js/custom.js
@@ -0,0 +1,190 @@
+(function($) { 
+		  
+$(document).ready(function(){
+	$('.slide').prepend('<div class="patternOverlay"></div>');	
+	
+	
+	
+	$('.skillbar2').each(function(){
+		
+		$(this).find('.progress-bar').animate({
+			width:$(this).attr('data-percent')
+		},5000);
+		$(this).find('.skill-percentage').animate({
+			left:$(this).attr('data-percent')
+		},6000);
+
+	});
+	
+	$('.skillbar2').each(function(){
+		$(this).find('.skill-percentage p').text($(this).attr('data-percent'));
+	});
+/*
+	$('.skillbar2').each(function () {
+		let percentage = parseInt($(this).attr('data-percent').replace("%", ""));
+		jQuery({ Counter: 0 }).animate({ Counter: percentage }, {
+		  duration: 1000,
+		  easing: 'swing',
+		  step: function () {
+			
+			
+			$(this).find('.skill-percentage p').text(Math.ceil(this.Counter));
+		  }
+		});
+	  });
+*/
+	
+	if($(window).width()>767){
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},200);
+		$('.navbar-wrapper').addClass('default-menu');
+		
+	}	
+	
+	$('#nav-show').click(function(x){
+		x.preventDefault();
+		$('.wrapper').removeClass('noGap');
+		$('.navbar-wrapper').animate({
+			opacity:1
+		},200);
+		$('.navbar-wrapper').removeClass('default-menu');
+		$(this).fadeOut();
+	});
+	
+	$('.closeMenu').click(function(x){
+		x.preventDefault();
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},50,function(){$('.navbar-wrapper').addClass('default-menu');});
+		
+		$('#nav-show').fadeIn();
+	});
+	
+	
+});	
+
+
+	
+/*===========================================================*/
+	/*	Preloader 
+/*===========================================================*/	
+//<![CDATA[
+	$(window).load(function() { // makes sure the whole site is loaded
+		$("#status").fadeOut(); // will first fade out the loading animation
+		$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+	})
+//]]>
+		  
+// for mobile nav js	
+$(window).load(function(){
+	$('button.navbar-toggle').click(function(){
+		$(this).toggleClass('active');
+		$('.navbar-collapse').slideToggle();										 
+	});					
+});
+// for banner height js
+var windowWidth = $(window).width();
+var windowHeight =$(window).height();
+$('.banner').css({'height':windowHeight });
+
+
+// for portfoli filter jquary
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    /*$container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });*/ 
+
+
+
+// for portfoli lightbox jquary
+jQuery(function() {
+	var $chosenSheet,
+	$stylesheets = $( "a[id^=theme-]" );
+	
+	// run rlightbox
+	$( ".lb" ).rlightbox();
+	$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
+});
+});
+
+// Somth page scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -80
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// Testimonial slider
+$(window).load(function(){
+  $('.flexslider').flexslider({
+	animation: "fade",
+	animationLoop: true,
+	slideshow: true,
+	pauseOnAction: false,
+	slideshowSpeed: 7000,
+	controlNav: true,
+	start: function(slider){
+	  $('body').removeClass('loading');
+	}
+  });
+});
+
+
+// we worked slider
+$(window).load(function(){
+      $('.worklogo').flexslider({
+        animation: "slide",
+		slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        
+      });
+    });
+
+// Skill set 
+
+$( window ).scroll(function() {
+	if($(window).scrollTop() > ($("header").scrollTop()+$("header").innerHeight())-80){
+		$(".menu").addClass("nav-bg-show");
+	}
+	else{
+		$(".menu").removeClass("nav-bg-show");
+	}
+  });
+
+
+
+}(jQuery));
---
+++ Folio - Personal Portfolio Template/js/edit.portfolio.js
@@ -0,0 +1,128 @@
+imgArr=[
+            //Logos
+            { filterType : "logos", imgPath: "1.jpg", imgTitle: "Logos 1", },
+            { filterType : "logos", imgPath: "2.jpg", imgTitle: "Logos 2", },
+            { filterType : "logos", imgPath: "3.jpg", imgTitle: "Logos 3", },
+            { filterType : "logos", imgPath: "4.jpg", imgTitle: "Logos 4", },
+            { filterType : "logos", imgPath: "5.jpg", imgTitle: "Logos 5", },
+            { filterType : "logos", imgPath: "6.jpg", imgTitle: "Logos 6", },
+            { filterType : "logos", imgPath: "7.jpg", imgTitle: "Logos 7", },
+            { filterType : "logos", imgPath: "8.jpg", imgTitle: "Logos 8", },
+            { filterType : "logos", imgPath: "9.jpg", imgTitle: "Logos 9", },
+            { filterType : "logos", imgPath: "10.jpg", imgTitle: "Logos 10", },
+            { filterType : "logos", imgPath: "11.jpg", imgTitle: "Logos 11", },
+            { filterType : "logos", imgPath: "12.jpg", imgTitle: "Logos 12", },
+            //Websites
+            { filterType : "websites", imgPath: "1.jpg", imgTitle: "Websites 1", },
+            { filterType : "websites", imgPath: "2.jpg", imgTitle: "Websites 2", },
+            { filterType : "websites", imgPath: "3.jpg", imgTitle: "Websites 3", },
+            { filterType : "websites", imgPath: "4.jpg", imgTitle: "Websites 4", },
+            { filterType : "websites", imgPath: "5.jpg", imgTitle: "Websites 5", },
+            { filterType : "websites", imgPath: "6.jpg", imgTitle: "Websites 6", },
+            { filterType : "websites", imgPath: "7.jpg", imgTitle: "Websites 7", },
+            { filterType : "websites", imgPath: "8.jpg", imgTitle: "Websites 8", },
+            { filterType : "websites", imgPath: "9.jpg", imgTitle: "Websites 9", },
+            { filterType : "websites", imgPath: "10.jpg", imgTitle: "Websites 10", },
+            { filterType : "websites", imgPath: "11.jpg", imgTitle: "Websites 11", },
+            { filterType : "websites", imgPath: "12.jpg", imgTitle: "Websites 12", },
+            { filterType : "websites", imgPath: "13.jpg", imgTitle: "Websites 1", },
+            { filterType : "websites", imgPath: "14.jpg", imgTitle: "Websites 2", },
+            { filterType : "websites", imgPath: "15.jpg", imgTitle: "Websites 3", },
+            { filterType : "websites", imgPath: "16.jpg", imgTitle: "Websites 4", },
+            { filterType : "websites", imgPath: "17.jpg", imgTitle: "Websites 5", },
+            { filterType : "websites", imgPath: "18.jpg", imgTitle: "Websites 6", },
+            { filterType : "websites", imgPath: "19.jpg", imgTitle: "Websites 7", },
+            { filterType : "websites", imgPath: "20.jpg", imgTitle: "Websites 8", },
+            //BusinessCards
+            { filterType : "businesscards", imgPath: "1.jpg", imgTitle: "Business Cards 1", },
+            { filterType : "businesscards", imgPath: "2.jpg", imgTitle: "Business Cards 2", },
+            { filterType : "businesscards", imgPath: "3.jpg", imgTitle: "Business Cards 3", },
+            { filterType : "businesscards", imgPath: "4.jpg", imgTitle: "Business Cards 4", },
+            { filterType : "businesscards", imgPath: "5.jpg", imgTitle: "Business Cards 5", },
+            { filterType : "businesscards", imgPath: "6.jpg", imgTitle: "Business Cards 6", },
+            { filterType : "businesscards", imgPath: "7.jpg", imgTitle: "Business Cards 7", },
+            { filterType : "businesscards", imgPath: "8.jpg", imgTitle: "Business Cards 8", },
+            { filterType : "businesscards", imgPath: "9.jpg", imgTitle: "Business Cards 9", },
+            { filterType : "businesscards", imgPath: "10.jpg", imgTitle: "Business Cards 10", },
+            { filterType : "businesscards", imgPath: "11.jpg", imgTitle: "Business Cards 11", },
+            { filterType : "businesscards", imgPath: "12.jpg", imgTitle: "Business Cards 12", },
+            { filterType : "businesscards", imgPath: "13.jpg", imgTitle: "Business Cards 13", },
+            //E-Flyer
+            { filterType : "eflyers", imgPath: "1.jpg", imgTitle: "E-Flyer 1", },
+            { filterType : "eflyers", imgPath: "2.jpg", imgTitle: "E-Flyer 2", },
+            { filterType : "eflyers", imgPath: "3.jpg", imgTitle: "E-Flyer 3", },
+            { filterType : "eflyers", imgPath: "4.jpg", imgTitle: "E-Flyer 4", },
+            { filterType : "eflyers", imgPath: "5.jpg", imgTitle: "E-Flyer 5", },
+            { filterType : "eflyers", imgPath: "6.jpg", imgTitle: "E-Flyer 6", },
+            { filterType : "eflyers", imgPath: "7.jpg", imgTitle: "E-Flyer 7", },
+            { filterType : "eflyers", imgPath: "8.jpg", imgTitle: "E-Flyer 8", },
+            { filterType : "eflyers", imgPath: "9.jpg", imgTitle: "E-Flyer 9", },
+            { filterType : "eflyers", imgPath: "10.jpg", imgTitle: "E-Flyer 10", },
+            { filterType : "eflyers", imgPath: "11.jpg", imgTitle: "E-Flyer 11", },
+            { filterType : "eflyers", imgPath: "12.jpg", imgTitle: "E-Flyer 12", },
+            { filterType : "eflyers", imgPath: "13.jpg", imgTitle: "E-Flyer 13", },
+            { filterType : "eflyers", imgPath: "14.jpg", imgTitle: "E-Flyer 14", },
+            { filterType : "eflyers", imgPath: "15.jpg", imgTitle: "E-Flyer 15", },
+            { filterType : "eflyers", imgPath: "16.jpg", imgTitle: "E-Flyer 16", },
+            { filterType : "eflyers", imgPath: "17.jpg", imgTitle: "E-Flyer 17", },
+            { filterType : "eflyers", imgPath: "18.jpg", imgTitle: "E-Flyer 18", },
+            { filterType : "eflyers", imgPath: "19.jpg", imgTitle: "E-Flyer 19", },
+            { filterType : "eflyers", imgPath: "20.jpg", imgTitle: "E-Flyer 20", },
+            { filterType : "eflyers", imgPath: "21.jpg", imgTitle: "E-Flyer 21", },
+            { filterType : "eflyers", imgPath: "22.jpg", imgTitle: "E-Flyer 22", },
+            //Flyer
+            { filterType : "flyers", imgPath: "1.jpg", imgTitle: "Flyer 1", },
+            { filterType : "flyers", imgPath: "2.jpg", imgTitle: "Flyer 2", },
+            { filterType : "flyers", imgPath: "3.jpg", imgTitle: "Flyer 3", },
+            { filterType : "flyers", imgPath: "4.jpg", imgTitle: "Flyer 4", },
+            { filterType : "flyers", imgPath: "5.jpg", imgTitle: "Flyer 5", },
+            { filterType : "flyers", imgPath: "6.jpg", imgTitle: "Flyer 6", },
+            { filterType : "flyers", imgPath: "7.jpg", imgTitle: "Flyer 7", },
+            { filterType : "flyers", imgPath: "8.jpg", imgTitle: "Flyer 8", },
+            { filterType : "flyers", imgPath: "9.jpg", imgTitle: "Flyer 9", },
+            { filterType : "flyers", imgPath: "10.jpg", imgTitle: "Flyer 10", },
+            { filterType : "flyers", imgPath: "11.jpg", imgTitle: "Flyer 11", },
+            { filterType : "flyers", imgPath: "12.jpg", imgTitle: "Flyer 12", },
+            //Poster
+            { filterType : "poster", imgPath: "1.jpg", imgTitle: "Poster 1", },
+            { filterType : "poster", imgPath: "2.jpg", imgTitle: "Poster 2", },
+            { filterType : "poster", imgPath: "3.jpg", imgTitle: "Poster 3", },
+            //Book Cover
+            { filterType : "bookcover", imgPath: "1.jpg", imgTitle: "Book Cover 1", },
+            { filterType : "bookcover", imgPath: "2.jpg", imgTitle: "Book Cover 2", },
+            //Panaflex
+            { filterType : "panaflex", imgPath: "1.jpg", imgTitle: "Panaflex 1", }
+]
+
+
+
+imgArr=shuffleAds(imgArr);
+
+
+function shuffleAds(arr)
+{
+  for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+  return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+imgArr.forEach(function(item,index){
+    thumnailName = imgArr[index].imgPath.split(".")[0];
+    $("ul.portfolioContainer").append('<li class="filter '+imgArr[index].filterType+' col-xs-6 col-sm-4 col-md-3 col-lg-3">'+
+                    '<div class="lightCon"> <span class="hoverBox"> <span class="smallIcon"> <a rel="lightbox-demo" href="images/projects/big-images/'+(imgArr[index].filterType).toLowerCase()+"/"+imgArr[index].imgPath+'" title="Project '+imgArr[index].imgTitle+'" class="zoom lb lb_warsaw1"><i class="fa fa-plus fa-2x"></i></a> </span> </span> <img src="images/projects/thumb-images/'+(imgArr[index].filterType).toLowerCase()+"/thumb"+thumnailName+'.jpg" alt="" > </div>'+
+                   '</li>' )
+      
+
+});
